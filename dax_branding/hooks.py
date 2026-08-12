3ce48838f2722ae6c17472bc0447b6b7ee291ada
Usman-Tariq777 <usmantariq2511@gmail.com>
2026-08-12 10:38:42 +0000

    set the email template

--- dax_branding/hooks.py
+++ dax_branding/hooks.py
@@ -42,7 +42,7 @@ web_include_js = "/assets/dax_branding/js/dax_branding.js"
 # include js in page
 # page_js = {"page" : "public/js/file.js"}
 
-# include js in doctype views
+include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
